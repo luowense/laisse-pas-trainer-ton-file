969d26952f7343b435b5209df30987a16c8f71ba
Laurent Segura <laurent.roger.segura@gmail.com>
2019-11-01 15:18:06 +0100

    quete terminee

---
+++ upload.php
@@ -0,0 +1,93 @@
+<?php
+
+//section to create the updating pictures
+if (!empty($_FILES['files']['name'][0])) {
+    $files = $_FILES['files'];
+    $uploaded = [];
+    $failed = [];
+    $allowed = ['jpg', 'png', 'gif'];
+
+    foreach ($files['name'] as $position => $file_name) {
+
+        $file_tmp = $files['tmp_name'][$position];
+        $file_size = $files['size'][$position];
+        $file_error = $files['error'][$position];
+
+        $file_ext = explode('.', $file_name);
+        $file_ext = strtolower(end($file_ext));
+
+        if (in_array($file_ext, $allowed)) {
+            if ($file_error === 0) {
+                if ($file_size <= 2097152 && $file_size > 0) {
+                    $fileNameNew = uniqid('image', true) . '.' . $file_ext;
+                    $fileDestination = 'uploads/' . $fileNameNew;
+
+                    if (move_uploaded_file($file_tmp, $fileDestination)) {
+                        $uploaded[$position] = $fileDestination;
+                    } else {
+                        $failed[$position] = "[{$file_name}] failed to upload";
+                    }
+                } else {
+                    $failed[$position] = "[{$file_name}] is too large";
+                }
+            } else {
+                $failed[$position] = "[{$file_name}] errored with code {$file_error}";
+            }
+        } else {
+            $failed[$position] = "[{$file_name}] file extension {$file_ext} is not allowed";
+        }
+        //print_r($file_ext);
+    }
+
+    if (!empty($uploaded)) {
+        //print_r($uploaded);
+    }
+
+    if (!empty($failed)) {
+        //print_r($failed);
+    }
+}
+
+//section to delete pictures
+
+if(!empty($_POST['filename'])){
+    $file = $_POST['filename'];
+    $path = "uploads/$file";
+    if(file_exists($path)){
+        unlink($path);
+    }
+}
+?>
+
+<html>
+<head>
+    <title>Results</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<?php
+$directoryPath = 'uploads/';
+if (is_dir($directoryPath)) {
+    $files = scandir($directoryPath);
+
+    foreach ($files as $key => $file) {
+        if (strlen($file) > 5) {
+            echo "<div class=\"card\" style=\"width: 18rem;\">
+  <img src=\"uploads/$file\" class=\"card-img-top\" alt=\"...\">
+  <div class=\"card-body\">
+    <h5 class=\"card-title\">$file</h5>
+    <form action='upload.php' method='POST'>
+    <button type='submit' name='filename' value='$file' class=\"btn btn-primary\">Delete</button>
+    </form>
+  </div>
+</div>";
+        }
+    }
+};
+if (!empty($_GET['delete']) && strlen($_GET['delete'] > 1)) {
+    unlink($file);
+}
+?>
+</body>
+</html>
